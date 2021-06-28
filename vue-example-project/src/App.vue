2db11f2b4435c85f5f6881682e945424895bd0f9
juhee <qkr30510@naver.com>
2021-06-28 23:28:53 +0900

    뷰설치

---
+++ vue-example-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+      <h1>지도</h1>
+      <input type="text" placeholder="검색어를 입력하세요">
+      <buttom>검색</buttom>      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
